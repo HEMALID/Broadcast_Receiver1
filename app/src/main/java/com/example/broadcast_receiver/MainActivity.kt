170287c64811bc0e613d293facda966a3c2eb2b1
Hemali <hemalidalsaniya56@gmail.com>
2022-02-02 11:03:04 +0530

    hello

---
+++ app/src/main/java/com/example/broadcast_receiver/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.broadcast_receiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangeReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        receiver = AirplaneModeChangeReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+}
